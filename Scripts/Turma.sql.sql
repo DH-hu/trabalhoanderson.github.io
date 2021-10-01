cdfcf220892b33c3717ea9595d07f5f86e0881b6
DH-hu <dhavy.dantas@unigranrio.br>
2021-10-01 15:47:25 -0300

    Atualizacao dos Scripts

--- Scripts/Turma.sql.sql
+++ Scripts/Turma.sql.sql
@@ -5,3 +5,14 @@ cod_turma serial primary key,
 dat_inicio date,
 num_turma varchar (20),
 sigla varchar (20));
+
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('3', '26/4/2009', 1, 'US');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('210', '6/6/2014', 2, 'UA');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('9400', '8/11/2007', 3, 'CN');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('80', '7/1/2016', 4, 'CN');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('7305', '6/2/2005', 5, 'CN');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('77367', '4/1/2019', 6, 'NL');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('21942', '22/3/2017', 7, 'CI');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('4', '4/9/2014', 8, 'PL');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('5', '30/6/2008', 9, 'CN');
+insert into turma (cod_turma, dat_inicio, num_turma, sigla) values ('350', '27/6/2010', 10, 'ID');
--- Scripts/professor.sql.sql
+++ Scripts/professor.sql.sql
@@ -5,3 +5,14 @@ cod_professor serial primary key,
 nome varchar (20),
 rg varchar (20),
 dt_nascimento date);	
+
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (1, 'Inga Gowanlock', '242-60-6332', '18/8/1973');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (2, 'Cassaundra Volet', '590-80-2356', '14/8/1975');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (3, 'Brew Keays', '738-83-9160', '18/5/1976');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (4, 'Bertrand Pulhoster', '479-21-7731', '26/4/1979');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (5, 'Suzanna Jeskin', '210-05-0731', '26/9/1979');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (6, 'Liz Allaker', '566-19-3487', '19/9/1967');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (7, 'Gardner Sergean', '350-57-7713', '24/6/1975');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (8, 'Bentley Hallibone', '320-44-6997', '24/10/1967');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (9, 'Hannie Parratt', '788-95-3816', '8/1/1975');
+insert into professor (cod_professor, nome, rg, dt_nascimento) values (10, 'Devlen Struther', '882-65-4841', '30/12/1977');
